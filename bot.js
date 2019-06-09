469b2091690f14503fd5a60424520bb0cd14d64e
pramirez2019 <51500065+pramirez2019@users.noreply.github.com>
2019-06-09 17:00:14 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,27 +1,47 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
+const channel = new Discord.Channel();
 
- 
+
+
 
 client.on('ready', () => {
-
-    console.log('I am ready!');
-
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
- 
-
-client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
-});
-
+//client.on means the bot is listening
+client.on('message', msg => {
+    var args = msg.content;
+    var ps4games = ["RB6 Siege ", " Dead by daylight ", " Overwatch ", " MK11 ", " MK10 "];
+    switch (args) {
+        case '!dbd':
+            msg.reply('Lets play! <:eymario:558854493567975435>');
+        break;
+        case '!gamesid':
+            msg.reply('PSN:patricio_tv, steam: reloadedantrax <:eymario:558854493567975435>');
+            break;
+        case '!ps4games':
+            msg.reply(ps4games.toString()+":joystick:");
+            break;
+        case '!pong':
+            msg.channel.send('pong');
+            break;
+        case '!delete':
+            msg.channel.delete();
+            break;
+        case '!bulk':
+            msg.channel.bulkDelete(50)
+                .then(message => console.log(`Bulk deleted ${message.size} messages`))
+                .catch(console.error);
+       break;
+       case '!user':
+            msg.channel.send(`Your username: ${msg.author.username}\nYour ID: ${msg.author.id}`);
+            break;
+       case '!server':
+            msg.channel.send(`This server's name is: ${msg.guild.name}\nTotal members: ${msg.guild.memberCount}`);
+       break;
+    }
+})
  
 
 // THIS  MUST  BE  THIS  WAY
