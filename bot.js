0836a2cf3d0e14e4c02871a04ed056965a5e38e4
pramirez2019 <51500065+pramirez2019@users.noreply.github.com>
2019-06-11 00:02:31 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 //THIS IS TEST
-const config = require('./config.json'); //configuration files come in for storing static data that can be easily updated in a single place
+//const config = require('./config.json'); //configuration files come in for storing static data that can be easily updated in a single place
+const { prefix, token } = require('./config.json');//Using prefixes({prefix})stored on 
 const Discord = require('discord.js'); // require the discord.js module
 const client = new Discord.Client(); // create a new Discord client
 const channel = new Discord.Channel();
