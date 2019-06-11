444c9297b3a9ebc8806b83d340b87f767f94abf3
pramirez2019 <51500065+pramirez2019@users.noreply.github.com>
2019-06-10 22:07:19 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -67,5 +67,5 @@ client.on('message', msg => {
  
 
 // THIS  MUST  BE  THIS  WAY
-
+// login to Discord with your app's token
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
