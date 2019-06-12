62a4b92bdfe8ed0662a4dbe7c169796ee5bf7d42
pramirez2019 <51500065+pramirez2019@users.noreply.github.com>
2019-06-12 00:45:57 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -68,9 +68,9 @@ client.on('message', msg => {
                   return msg.channel.send(`Please provide any arguments, ask Skynet !skynet-info... ${msg.author}!`);
               } else if (args[0] === 'user') {
                   return msg.channel.send(`Your username: ${msg.author.username}\nYour ID: ${msg.author.id}`);
-            // }  else if (args[0] === 'discord') {
+            }  else if (args[0] === 'discord') {
               //fetchclient.fetchInvite('https://discord.gg/5FTJFDt')
-                //return msg.channel.send(`Join our machines army with this invitation: https://discord.gg/5FTJFDt \nCode:`));   
+                return msg.channel.send(`Join our machines army with this invitation: https://discord.gg/5FTJFDt \nCode:`));   
              } else if (args[0] === 'server' ) {
               return msg.channel.send(`Server's name is: ${msg.guild.name}\nTotal members: ${msg.guild.memberCount}`);                 
           }		    
