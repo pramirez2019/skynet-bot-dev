f065c7cbba32331f4a30013c6a7cc2f1350105d2
pramirez2019 <51500065+pramirez2019@users.noreply.github.com>
2019-06-12 00:06:29 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -70,7 +70,7 @@ client.on('message', msg => {
                   return msg.channel.send(`Your username: ${msg.author.username}\nYour ID: ${msg.author.id}`);
               } else if (args[0] === 'discord') {
                  client.fetchInvite('https://discord.gg/5FTJFDt')
-                .then(return invite => msg.channel.send(`Join our machines army with this invitation: https://discord.gg/5FTJFDt \nCode: ${invite.code}`))
+                .then(invite => msg.channel.send(`Join our machines army with this invitation: https://discord.gg/5FTJFDt \nCode: ${invite.code}`))
                 .catch(console.error);
               } else if (args[0] === 'server' ) {
                    return msg.channel.send(`Server's name is: ${msg.guild.name}\nTotal members: ${msg.guild.memberCount}`);                 
