06e8bd8ef594b4abcf0665ba4bfe9f8d4d281e8f
pramirez2019 <51500065+pramirez2019@users.noreply.github.com>
2019-06-23 21:50:18 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,26 @@
 const { prefix } = require('./config.json');//Using prefixes({prefix})stored on config.json file. configuration files come in for storing static data that can be easily updated in a single place
 const Discord = require('discord.js'); // require the discord.js module
 const client = new Discord.Client(); // create a new Discord client
-//const auth = require('./auth.json');  // Enable this for local testing
+client.commands = new Discord.Collection();
+const auth = require('./auth.json');  // Enable this for local testing
 const channel = new Discord.Channel();
+const fs = require('fs'); // fs is Node's native file system module.
+const cooldowns = new Discord.Collection(); //Collection for cooldowns
+const util = require('util')
+//const greatings = require('./commands/greetings.js'); //Calling the greetings.js module. and assigning to variable Testing only
+//const pong = require('./commands/pong.js');
+
+ /*The fs.readdirSync() method will return an ARRAY of all the file names in that directory,
+ e.g. ['ping.js', 'beep.js']. The filter is there to make sure any non-JS files are left out of the array.
+ With that array, you can loop over it and dynamically */
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+console.log(`Command read:  ${commandFiles}`);
+//Loading using for all my .js command from commandFiles to file.
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
@@ -17,101 +35,76 @@ client.on('ready', () => {
 
 //client.on means the bot is listening
 client.on('message', msg => {
-    let args = msg.content.slice(prefix.length).split(/ +/); //Slices off the prefix entirely and then splits it into an array by spaces. / +/ to avoid issues with spaces
-    const command = args.shift().toLowerCase(); //Variable by calling args.shift(), which will take the first element in array and return it while also removing it from the original array(so that you dont have the command name string inside the args array).
-    let ps4games = ["Patricio's PS4 games: RB6 Siege ", " Dead by daylight ", " Overwatch ", " MK11 ", " MK10 ", "and more"];
-    let pcgames = ["Patricio's PC games: Dead by daylight ", " Overwatch ", " Dragon Ball figther Z ", " Hollow knight ", "and more"];
-    let switchgames = ["Patricio's Nintendo games: Fallout Shelter ", " Super Smash Bros Ultimate ", " Mario Kart 8 ", " Just Dance 2019 ", "and more"];
+    //Slices off the prefix entirely and then splits it into an array by spaces. / +/ to avoid issues with spaces
+    let args = msg.content.slice(prefix.length).split(/ +/);
+    //Variable by calling args.shift(), which will take the first element in array and return it while also removing it from the original array(so that you dont have the command name string inside the args array).
+	  const commandName = args.shift().toLowerCase();
 
-    if (!msg.content.startsWith(prefix) || !msg.author.bot) {  // ||or && and ! not
-        //Command variable only have the word 'skynet-info' because we shift.
-        if (command === 'skynet-info') {
-            //args  variable only have the word ! because we split
-            if (!args.length) {  
-                return msg.channel.send(`Please provide any arguments ${msg.author}\nArguments: dbd, tags, info\nExample: !skynet-info dbd`);
-            } else if (args[0] === 'dbd') {
-                return msg.reply('Commands available for "!dbd": play, lup, ldown, ping');
-            } else if (args[0] === 'tags') {
-                return msg.reply('Commands available for "!tags": gamertags, pcgames, switchgames');
-            } else if (args[0] === 'info') {
-                return msg.reply('Commands available for "!info": user, discord, server');
-            }
-            msg.channel.send(`Human this argument is incorrect for skynet-info command, try again human: ${args[0]}`);
-        }
-        //Command variable only have the word 'dbd' because we shift.
-        if (command === 'dbd') { 
-            //args  variable only have the word ! because we split
-            if (!args.length) {  
-                return msg.channel.send(`Please provide any arguments ${msg.author}!\nArguments: play, lup, ping\nExample: !dbd + ARGUMENT. `);
-            } else if (args[0] === 'play') {
-                return msg.reply('Lets play! <:eymario:558854493567975435>');
-            } else if (args[0] === 'lup') {
-                return msg.channel.send(`You leveled up, hype!!!  :sunglasses:  ${msg.author}!`);
-            } else if (args[0] === 'ldown') {
-                return msg.channel.send(`Rank down, why!!!  :worried:  ${msg.author}!`);
-            } else if (args[0] === 'ping') {
-                const taggedUser = msg.mentions.users.first();
-                if (!msg.mentions.users.size) {
-                    return msg.reply('Human you need to tag a user in order to use this command');
-                }
-                return msg.channel.send(`Do you want to play some rounds? ${taggedUser.username}`);
-            }
-            msg.channel.send(`This argument is incorrect for dbd command, try again human: ${args[0]}`);
-        }
-        //Command variable only have the word 'arg-info' because we shift.
-        if (command === 'tags') {
-            //args  variable only have the word ! because we split
-            if (!args.length) {  
-                return msg.channel.send(`Please provide any arguments ${msg.author}!\nArguments: gamertags, pcgames, ps4games, switchgames\nExample: !tags + ARGUMENT.`);
-            } else if (args[0] === 'gamertags') {
-                return msg.reply('PSN:patricio_tv, Steam: reloadedantrax, Nintendo Switch: SW-0279-1159-3994 <:eymario:558854493567975435>');
-            } else if (args[0] === 'pcgames') {
-                return msg.reply(pcgames.toString() + ":joystick:");
-            } else if (args[0] === 'ps4games') {
-                return msg.reply(ps4games.toString() + ":joystick:");
-            } else if (args[0] === 'switchgames') {
-                return msg.reply(switchgames.toString() + ":joystick:");
-            }
 
-            msg.channel.send(`This argument is incorrect for tags command, try again human: ${args[0]}`);
-        }
-        //Command variable only have the word 'arg-info' because we shift.
-        if (command === 'info') {  
-            //args  variable only have the word ! because we split
-            if (!args.length) {  
-                return msg.channel.send(`Please provide any arguments ${msg.author}!\nArguments: user, discord, server\nExample: !info + ARGUMENT.`);
-            } else if (args[0] === 'user') {
-                return msg.channel.send(`Your username: ${msg.author.username}\nYour ID: ${msg.author.id}`);
-            } else if (args[0] === 'discord') {
-                client.fetchInvite('https://discord.gg/5FTJFDt');
-                return msg.channel.send(`Join our machines army with this invitation: https://discord.gg/5FTJFDt \nCode: `);
-            } else if (args[0] === 'server') {
-                return msg.channel.send(`Server's name is: ${msg.guild.name}\nTotal members: ${msg.guild.memberCount}`);
-            }
-            msg.channel.send(`This argument is incorrect for info command, try again human: ${args[0]}`);
-        }
-        //Command variable only have the word 'arg-info' because we shift.
-        if (command === 'admin') {  
-            //args  variable only have the word ! because we split
-            if (!args.length) {  
-                return msg.channel.send(`Please provide any arguments ${msg.author}!\nArguments: ping, delete, bulk, botinvite\nExample: !admin + ARGUMENT.`);
-            } else if (args[0] === 'ping') {
-                return msg.channel.send('Im here human, what do you need?');
-            } else if (args[0] === 'delete') {
-                return msg.channel.delete();
-            } else if (args[0] === 'bulk') {
-                return msg.channel.bulkDelete(50)
-                    .then(message => msg.channel.send(`Bulk deleted ${message.size} messages`))
-                    .catch(console.error);
-            } else if (args[0] === 'botinvite') {
-                     client.generateInvite(['SEND_MESSAGES', 'MANAGE_GUILD', 'MENTION_EVERYONE'])
-                    .then(link => msg.channel.send(`Skynet bot invite link: ${link}`))
-                    .catch(console.error);
-            }
-            msg.channel.send(`This argument is incorrect for admin command, try again human: ${args[0]}`);
-        }
+    /*If there isn't a command with that name, exit early. If there is, .get() the command and call its .execute() method while passing
+		in your msg and args variables as the method arguments.
+		In case something goes wrong, log the error and report back to the member to let them know.
+		Whenever you want to add a new command, you simply make a new file in your commands directory, name it what you want, and
+		then do what you did for the other commands.*/
+		if (!client.commands.has(commandName)) return;
 
-    }        
+		 //Loading the commands on command variable using get and later execute
+		const command = client.commands.get(commandName);
+
+		if (!msg.content.startsWith(prefix) || !msg.author.bot) {  // ||or && and ! not
+
+			 //Remember command.name is a property of the object. You need to declare it in the module to avoid undefined
+				if (!cooldowns.has(command.name)) {
+						cooldowns.set(command.name, new Discord.Collection());
+				}
+				// Now variable with the current time
+				const now = Date.now();
+				// .Get()s The Collection for the triggered command.
+				const timestamps = cooldowns.get(command.name);
+				//CooldownAmount variable that gets the necessary cooldown amount. 3*1000 = 3000 ms = 3 segs
+				const cooldownAmount = (command.cooldown || 5) * 1000;
+				//If statement to check if the timestamps Collection has the author ID in it yet.
+				if (timestamps.has(msg.author.id)) {
+						/*Since the timestamps Collection has the author ID in it,
+						 you .get() it and then sum it up with the cooldownAmount variable,
+						 in order to get the correct expiration timestamp*/
+						const expirationTime = timestamps.get(msg.author.id) + cooldownAmount;
+						/*If the expirationTime has not passed, return a message letting the user know
+						 how much time is left until they can use that command again*/
+						if (now < expirationTime) {
+								//TimeLeft variable time with the remaining time to use the command again
+								const timeLeft = (expirationTime - now) / 1000;
+								return msg.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing \`${command.name}\` the command.`);
+						}
+				}
+				/*if the timestamps Collection doesn't have the message author's ID (or if the author ID
+				 did not get deleted as planned), .set() the author ID with the current timestamp and create a setTimeout()
+				 to automatically delete it after the cooldown period has passed
+				*/
+
+				timestamps.set(msg.author.id, now);
+				setTimeout(() => timestamps.delete(msg.author.id), cooldownAmount);
+				}
+
+    try {
+				command.execute(msg, args, client);
+    } catch (error) {
+           console.error(error);
+        msg.reply('there was an error trying to execute that command!');
+    }
+
+		if (command.args && !args.length) {
+        let reply = `You didn't provide any arguments, ${msg.author}!`;
+
+        /*Use an if statement to check if the usage property exists (and is truthy) first,
+				so that you don't accidentally end up with undefined in the reply string (in the case that you forget to properly supply the property in your command file,
+				or some similar incident). */
+
+				if (command.usage) {
+					reply += `\nHuman the proper usage would be: \`${prefix}${command.name} ${command.usage}\`\nARGUMENTS: ${command.arguments}\n`;
+				}
+				return msg.channel.send(reply);
+			}
 })
 //ENABLE THIS FOR WEB SERVICE ONLY!!!
 // THIS  MUST  BE  THIS  WAY
